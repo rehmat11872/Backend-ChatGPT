e84cae9aea0c74b5c4c98189bae7c892e314f2c5
rehmat11872 <raorehmat11@gmail.com>
2023-12-22 02:55:14 +0500

    static files

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -41,3 +43,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
