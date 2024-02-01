7f62880114f22864557ec8c88198e4c18db8fd2a
rehmat11872 <raorehmat11@gmail.com>
2024-02-01 17:31:21 +0500

    add template

--- project/settings.py
+++ project/settings.py
@@ -88,7 +88,8 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates/',],
+        # 'DIRS': [BASE_DIR, 'templates/',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
