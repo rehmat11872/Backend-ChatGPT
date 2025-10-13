923abe570bfdc55e4af776d4fecb9fef4c495412
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2025-10-13 23:30:41 +0500

    Fixed indentation issue

--- project/settings.py
+++ project/settings.py
@@ -302,8 +302,6 @@ CORS_ORIGIN_WHITELIST = (
 # ===========================================
 
 REST_FRAMEWORK = {
-
-=======
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",  
     ],
